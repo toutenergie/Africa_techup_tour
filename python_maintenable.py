d2995aa2092d476e97b4dbf7796170eaa1cda399
Abdoulaye FAYE <abdoulaye7.faye@uadb.edu.sn>
2024-05-11 22:17:39 +0000

    git add .

---
+++ python_maintenable.py
@@ -0,0 +1,21 @@
+def travel_from_to_earth_to_ganymede():
+    """Commence l'aventure, de la terre à Ganymède."""
+    get_on_the_ship()
+    take_off_ship()
+ 
+    # code pour voyager dans l'espace
+ 
+    land_ship()
+    exit_the_ship()
+    open_the_hold()
+ 
+def travel_from_ganymede_to_kepler():
+    """Voyage de Ganymède à Kepler 438 b..."""
+    get_on_the_ship()
+    take_off_ship()
+ 
+    # code pour voyager dans l'espace
+ 
+    land_ship()
+    exit_the_ship()
+    open_the_hold()
